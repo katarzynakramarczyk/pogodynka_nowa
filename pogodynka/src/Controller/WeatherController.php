717541518c5f989459b3e8f6156b0a6012fcae95
katarzynakramarczyk <kasiakramarczyk@gmail.com>
2024-02-04 21:01:59 +0100

    lab-c-wersja 5 i nadal działa

--- pogodynka/src/Controller/WeatherController.php
+++ pogodynka/src/Controller/WeatherController.php
@@ -9,10 +9,12 @@ use Symfony\Component\Routing\Annotation\Route;
 class WeatherController extends AbstractController
 {
     #[Route('/weather', name: 'app_weather')]
-    public function index(): Response
+    public function city(): Response
     {
         return $this->render('weather/index.html.twig', [
             'controller_name' => 'WeatherController',
         ]);
     }
 }
+
+
